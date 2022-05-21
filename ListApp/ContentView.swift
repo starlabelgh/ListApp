ea6cce0253dad928d54df0f88fe4bdafc2ed922d
starlabelgh <hello@starlabel.co>
2022-05-21 12:21:23 +0000

    Initial Commit

---
+++ ListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListApp
+//
+//  Created by Moses Samanhyia on 21/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListApp/ListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListAppApp.swift
+//  ListApp
+//
+//  Created by Moses Samanhyia on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
